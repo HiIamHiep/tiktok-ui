826b9df7bf04aa362f36efabdb611db2e0a7f437
HiIamHiep <92458329+HiIamHiep@users.noreply.github.com>
2024-05-14 11:14:52 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,28 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import css from './Header.module.scss';
+import images from '~/assets/images';
+import { faMagnifyingGlass, faSpinner, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+
 function Header() {
     const cx = classNames.bind(css);
 
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faXmarkCircle} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </div>
     );
 }
